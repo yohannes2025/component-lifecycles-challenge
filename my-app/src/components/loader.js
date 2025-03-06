dcce456433f329d00402befa74b5136452980efb
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-06 07:14:43 +0300

    Update

--- my-app/src/components/loader.js
+++ my-app/src/components/loader.js
@@ -1,14 +1,9 @@
-/* Loader.module.css */
-.Loader {
-  border: 4px solid #f3f3f3;
-  border-top: 4px solid #3498db;
-  border-radius: 50%;
-  width: 30px;
-  height: 30px;
-  animation: spin 2s linear infinite;
-}
+// Loader.js
+import React from "react";
+import css from "./Loader.module.css";
 
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
+const Loader = () => {
+  return <div className={css.Loader}></div>;
+};
+
+export default Loader;
